b9fd05beda6999ab4d99d8e490daf31fd7da16a0
Hadrien Patte <hadrien.patte@protonmail.com>
2025-07-17 23:40:10 +0200

    test

--- .github/scripts/build-image.sh
+++ .github/scripts/build-image.sh
@@ -1,31 +1,16 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-IMAGE=$1
-
-echo "Building $IMAGE"
-
 DEFAULT_UBUNTU_VERSION="24.04"
 DEFAULT_CHISEL_VERSION="v1.2.0"
 
-REPOSITORY=$(jq -r '.repository' ./images/${IMAGE}/metadata.json)
-DESCRIPTION=$(curl -s "https://api.github.com/repos/${REPOSITORY}" | jq -r '.description | values')
-RELEASE_METADATA=$(curl -s "https://api.github.com/repos/${REPOSITORY}/releases/latest")
-SOURCE_DATE_EPOCH=$(date +%s -d $(echo ${RELEASE_METADATA} | jq -r '.created_at'))
-RELEASE=$(echo ${RELEASE_METADATA} | jq -r '.tag_name')
-VERSION=${RELEASE%%_*}
-VERSION=${VERSION#release-}
-VERSION=${VERSION#v}
-
-echo "Version $VERSION"
 if [[ -z $VERSION ]]; then
     echo "Failed to retrieve latest version for $IMAGE"
 else
+    echo "Building ${IMAGE}:${VERSION}"
     docker buildx build \
-        --platform linux/amd64,linux/arm64 \
+        --platform "${PLATFORM}" \
         --provenance=false \
-        --tag ghcr.io/${GITHUB_REPOSITORY_OWNER,,}/${IMAGE}:${VERSION} \
-        --tag ghcr.io/${GITHUB_REPOSITORY_OWNER,,}/${IMAGE}:latest \
         --build-arg RELEASE=${RELEASE} \
         --build-arg VERSION=${VERSION} \
         --build-arg UBUNTU_VERSION=${DEFAULT_UBUNTU_VERSION} \
@@ -38,8 +23,7 @@ else
         --label "org.opencontainers.image.version=${VERSION}" \
         --label "org.opencontainers.image.vendor=${GITHUB_REPOSITORY_OWNER}" \
         --label "org.opencontainers.image.title=${IMAGE}" \
-        --annotation "index:org.opencontainers.image.description=${DESCRIPTION}" \
-        --output "type=registry,name=ghcr.io/${GITHUB_REPOSITORY_OWNER,,}/${IMAGE}:${VERSION},rewrite-timestamp=true" \
-        --output "type=registry,name=ghcr.io/${GITHUB_REPOSITORY_OWNER,,}/${IMAGE}:latest,rewrite-timestamp=true" \
+        --output "type=image,name=ghcr.io/${GITHUB_REPOSITORY_OWNER,,}/${IMAGE}-test,rewrite-timestamp=true,push-by-digest=true" \
         - < images/${IMAGE}/Dockerfile
+        #--output "type=registry,name=ghcr.io/${GITHUB_REPOSITORY_OWNER,,}/${IMAGE}:latest,rewrite-timestamp=true" \
 fi
